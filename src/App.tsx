58a1d63cde34b9b34e4af1da51961a27098c4e82
Anton Mamontov <a.mamontov@p-s.kz>
2023-11-26 11:43:46 +0600

    route an param

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import BasePage from "./pages/BasePage";
 import TodoPage from "./pages/TodoPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 
 function App() {
+  const i = useParams();
+  console.log(i);
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
           <Route path="/" element={<BasePage />} />
-          <Route path="/pwa-todo/:id" element={<TodoPage />} />
+          <Route path="/:id" element={<TodoPage />} />
         </Routes>
       </header>
     </div>
