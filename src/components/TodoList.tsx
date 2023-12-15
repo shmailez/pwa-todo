63dabcd3cb03d0e6c8ca89ecb656350b534e4f62
Anton Mamontov <a.mamontov@p-s.kz>
2023-12-15 17:02:00 +0600

    modal backgroup correct

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,11 +4,16 @@ import { useAppSelector } from "../hooks";
 const TodoList: React.FC = () => {
   const list = useAppSelector((state) => state.todos.todos);
 
+  // const
+
   return (
     <ul className="generalColumn">
       {list.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
+      {/* {list.map((todo) => (
+        <li key={todo.id}>{todo.deadline}</li>
+      ))} */}
     </ul>
   );
 };
